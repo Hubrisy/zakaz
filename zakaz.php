9f577537606b1b960bd4fa75801cde3cf545e2e5
Vladimir <79149193+Hubrisy@users.noreply.github.com>
2024-03-27 14:50:42 +0200

    Update zakaz.php

--- zakaz.php
+++ zakaz.php
@@ -1,36 +1,26 @@
-<?php 
-$name = stripslashes(htmlspecialchars($_POST['name']));
-$phone = stripslashes(htmlspecialchars($_POST['phone']));
-// $city = stripslashes(htmlspecialchars($_POST['city']));
-// $post = stripslashes(htmlspecialchars($_POST['post']));
-$size = stripslashes(htmlspecialchars($_POST['size']));
-$color = stripslashes(htmlspecialchars($_POST['color']));
-$comment = stripslashes(htmlspecialchars($_POST['comment']));
+<!DOCTYPE html>
+<html lang="ru">
+<head>
 
+	<meta charset="UTF-8">
+	<title>Дякую за замовлення</title>
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400&amp;subset=cyrillic" rel="stylesheet">
+	<style>
+		a{color:#fff;text-decoration:none}.container{width:100%;height:100%;font-family:'Open Sans',sans-serif}.wrap{width:700px;margin:165px auto 0;text-align:center}.zag{font-size:29px;font-weight:400;margin-bottom:0;margin-top:23px;color:#38382F}.podzag{font-size:20px;color:#7F7F7F;font-weight:300;margin-top:12}.button{background:#38382F;padding:12px 25px;margin:0 auto}.bor{margin-top:45px}.img{width:100px;height:100px;background:url(http://i82.fastpic.ru/big/2016/1110/91/900e8526e8b44b8993f6e4ca19ce7891.png) center center no-repeat;margin:0 auto}
+	</style>
 
-if($_GET['product_id']){
-    $product_id = $_GET['product_id'];
-}else{
-    $product_id = $_POST['product_id'];
-}
-if(empty($name) || empty($phone)){
-echo '<h1 style="color:red;">Пожалуйста заполните все поля</h1>';
-echo '<meta http-equiv="refresh" content="2; url=http://'.$_SERVER['SERVER_NAME'].'">';
-}
-else{
-$subject = 'Універсальний ліхтар туриста'; // заголовок письмя
-$addressat = "kompot.andrik@gmail.com"; // Ваш Электронный адрес
-$success_url = './call.php?name='.$_POST['name'].'&phone='.$_POST['phone'].'&comment='.$_POST['comment'].'&size='.$_POST['size'].'&color='.$_POST['color'].'&time='.$_POST['Время_звонка'].'';
-$message = "ФИО: {$name}\nКонтактный телефон: {$phone}\nТовар: {$product_id}";
-$verify = mail($addressat,$subject,$message,"Content-type:text/plain;charset=utf-8\r\n");
-if ($verify == 'true'){
-    header('Location: '.$success_url);
-    echo '<h1 style="color:green;">Вітаю! Ваш заказ прийнятий!</h1>';
-    exit;
-}
-else 
-    {
-    echo '<h1 style="color:red;">Виникла помилка!</h1>';
-    }
-}
-?>
+</head>
+<body>
+	<div class="container">
+		<div class="wrap">
+			<div class="img"></div>
+			<p class="zag">Дякую. Ваша заявка успішно відправлена.</p>
+			<p class="podzag">Наш менеджер зв'яжеться з Вами найближчим часом.</p>
+			<div class="bor">
+				<a href="index.php" class="button">Повернутись на сайт</a>
+			</div> 
+		</div>
+	</div>
+
+</body>
+</html>
